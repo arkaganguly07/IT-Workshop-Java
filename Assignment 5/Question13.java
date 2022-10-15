79a8630fbf870fd3ee86bbe0e2f167d9e74fc3ae
Arka Ganguly <arkaganguly007@gmail.com>
2022-10-16 00:40:01 +0530

    Update Question13.java

--- Assignment 5/Question13.java
+++ Assignment 5/Question13.java
@@ -8,39 +8,146 @@ Create a subclass of this class SavingsAccount and add the following details:
  * Methods: (a)calculateAmount
  */
 
+import java.io.*;
+
 abstract class Accounts{
     double Balance;
-    int accountNumber = 329745612;
-    String accountHoldersName = "Yorrichi Tsugikuni", address = "Roy Bahadur Road";
+    long accountNumber;
+    String accountHoldersName = new String();
+    String address = new String();
 
-    abstract public double withdrawal();
-    abstract public double deposit();
-    public void display(double Balance, int accountNumber)
+    Accounts(long ac, String name, double bal, String add)
     {
-        System.out.println("Balance in the account number " + this.accountNumber + " : " + this.Balance);
+        accountNumber = ac;
+        Balance = bal;
+        accountHoldersName = name;
+        address = add;
+    }
+
+    abstract void withdrawal(double d);
+    abstract void deposit(double d);
+
+    void display()
+    {
+        System.out.println("Available balance: " + Balance);
     }
 }
 
 class SavingsAccount extends Accounts{
-    double rateOfInterest = 13;
+    final double rateOfInterest = 3.5;
 
-    @Override
-    public double deposit()
+    SavingsAccount(long ac, String name, double bal, String add)
     {
-        return 0;
+        super(ac, name, bal, add);
+    }
+    @Override
+    void withdrawal(double amt)
+    {
+        super.Balance -= amt;
     }
 
     @Override
-    public double withdrawal()
+    void deposit(double amt)
     {
-        return 0;
+        super.Balance += amt;
     }
 
-    public double calculateAmount()
+    void calculateAmount()
     {
-        return 0;
+        super.Balance += (rateOfInterest / 100) * super.Balance;
     }
 }
 
-public class Question13 {
+public class Question13{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String n = new String();
+        long acn;
+        double b, a;
+        String add = new String();
+        int ch;
+
+        System.out.println("Enter Account Number: ");
+        acn = Long.parseLong(br.readLine());
+
+        System.out.println("Enter Name: ");
+        n = br.readLine();
+
+        System.out.println("Enter Address: ");
+        add = br.readLine();
+
+        System.out.println("Enter Initial Amount: ");
+        b = Double.parseDouble(br.readLine());
+
+        SavingsAccount ob = new SavingsAccount(acn, n, b, add);
+        while (true)
+        {
+            System.out.println("1. Deposit\n2. Withdraw\n3. Display Balance\n0. Exit");
+            System.out.println("Enter your choice: ");
+
+            ch = Integer.parseInt(br.readLine());
+            switch (ch)
+            {
+                case 1:
+                    System.out.println("Enter Amount: ");
+                    a = Double.parseDouble(br.readLine());
+                    ob.deposit(a);
+                    break;
+
+                case 2:
+                    System.out.println("Enter Amount: ");
+                    a = Double.parseDouble(br.readLine());
+                    ob.withdrawal(a);
+                    break;
+
+                case 3:
+                    ob.calculateAmount();
+                    ob.display();
+                    break;
+
+                case 0:
+                    System.out.println("Thank, You.\nPlease visit again.");
+                    System.exit(0);
+
+                default:
+                    System.out.println("Your input is wrong.\nTry again.");
+            }
+        }
+    }
 }
+
+/*
+Output:
+Enter Account Number:
+5239578033
+Enter Name:
+Yorrichi Tsugikuni
+Enter Address:
+Sengoku Area
+Enter Initial Amount:
+500000
+1. Deposit
+2. Withdraw
+3. Display Balance
+0. Exit
+Enter your choice:
+1
+Enter Amount:
+1000000
+1. Deposit
+2. Withdraw
+3. Display Balance
+0. Exit
+Enter your choice:
+3
+Available balance: 1552500.0
+1. Deposit
+2. Withdraw
+3. Display Balance
+0. Exit
+Enter your choice:
+0
+Thank, You.
+Please visit again.
+ */
