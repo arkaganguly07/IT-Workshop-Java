a902fbfb6bc2c5e10a1f0c27a58dbd3d5a4d9fd4
Arka Ganguly <arkaganguly007@gmail.com>
2022-10-15 23:55:58 +0530

    Create B.java

---
+++ Assignment 6/Package/Subpackage/B.java
@@ -0,0 +1,13 @@
+/*
+Q1> Write a Java program to create package and subpackage.
+ */
+// Subpackage created
+
+package pack.package2;
+
+public class B{
+	public void display()
+	{
+		System.out.println("Full-Metal Alchemist");
+	}
+}
