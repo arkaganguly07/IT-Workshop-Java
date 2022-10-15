b96803eb0317a0376f2754069447f644a938ae26
Arka Ganguly <arkaganguly007@gmail.com>
2022-10-15 23:56:59 +0530

    Add files via upload

---
+++ Assignment 6/Package/A.java
@@ -0,0 +1,13 @@
+/*
+Q1> Write a Java program to create package and subpackage.
+ */
+// Package created
+
+package pack;
+
+public class A{
+	public void msg()
+	{
+		System.out.println("Hello Folks");
+	}
+}
