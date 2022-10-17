c4a8baf803f25b88852ef80b3dccfc8d903b0b4f
Arka Ganguly <arkaganguly007@gmail.com>
2022-10-17 09:43:19 +0530

    Update Question10.java

--- Assignment 6/Question10.java
+++ Assignment 6/Question10.java
@@ -15,6 +15,53 @@ class RangeException extends Exception{}
 public class Question10{
     public static void main(String[] args)
     {
+        String name;
+        int i, j;
+        double percentage;
+        int sum = 0;
+        int marks[] = new int[6];
 
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the name of the Student: ");
+        name = sc.nextLine();
+
+        System.out.println("Enter the number of subjects: ");
+        j = sc.nextInt();
+
+        try
+        {
+            int cnt = 0;
+
+            for (i = 0; i < j; ++i)
+            {
+                System.out.println("Enter the marks of the student: ");
+                marks[i] = sc.nextInt();
+                ++cnt;
+
+                if (marks[i] < 0)
+                    throw new RangeException();
+
+                else if (marks[i] > 50)
+                    throw new RangeException();
+
+            }
+
+            for (i = 0; i < j; ++i)
+                sum += marks[i];
+
+            percentage = sum / 300 * 100;
+        }
+
+        catch (RangeException r)
+        {
+            System.out.println("The marks are not in range");
+        }
+        
+        System.out.println("Total Percentage = " + percentage);
     }
 }
+
+/*
+Output:
+
+ */
